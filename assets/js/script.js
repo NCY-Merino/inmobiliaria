396c45cbca5cdd05d16ff00dc5c267b788853432
Nancy Merino <merino.catalina@gmail.com>
2024-11-10 22:15:39 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,291 @@
+const propiedades_alquiler = [
+    {
+        nombre: 'Activa Bezanilla',
+        src: './assets/img/alquiler_1.JPG',
+        descripcion: 'Edificio 100% dedicado al arriendo con gastos comunes fijos. Cuenta con administración y mantención profesional.',
+        ubicacion: 'Pje. Nueva Maruri 1833, Hospitales, Independencia, RM (Metropolitana)',
+        habitaciones: 2,
+        bano: 1,
+        costo: 380000,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Somma Inés de Suárez',
+        src: './assets/img/alquiler_2.JPG',
+        descripcion: 'Conoce nuestros espacios comunes y disfruta de una nueva experiencia en servicio. Sumamos conectividad y centralidad en el lugar más cool y cosmopolita de Santiago, con tintes financieros, empresariales, residenciales, cívicos y familiares.',
+        ubicacion: 'Avenida Pedro De Valdivia 2100 - 2400, Inés de Suárez, Providencia, RM (Metropolitana)',
+        habitaciones: 1,
+        bano: 1,
+        costo: 680520,
+        smoke: false,
+        pets: true
+    },
+    {
+        nombre: 'Somma Asturias - Edifiio Multifamily',
+        src: './assets/img/alquiler_3.JPG',
+        descripcion: 'Somma Asturias es un edificio Multifamily, los departamentos se encuentran equipados con cocina integrada y equipada, refrigerador, freezer, microondas, horno eléctrico, cocina encimera y campana extractora.',
+        ubicacion: 'Asturias 77, Metro Escuela Militar, Las Condes, RM (Metropolitana)',
+        habitaciones: 3,
+        bano: 2,
+        costo: 977760,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Oficina',
+        src: './assets/img/alquiler_4.JPG',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        ubicacion: 'Prieto Norte 303, Centro de Temuco, Temuco, La Araucanía',
+        habitaciones: 5,
+        bano: 3,
+        costo: 7599008,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Oficina',
+        src: './assets/img/alquiler_5.JPG',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        ubicacion: 'Prieto Norte 303, Centro de Temuco, Temuco, La Araucanía',
+        habitaciones: 5,
+        bano: 3,
+        costo: 56008,
+        smoke: true,
+        pets: false
+    }
+];
+
+const propiedades_venta = [
+    {
+        nombre: 'Gran Quincho Y Piscina - Como Nueva',
+        src: './assets/img/venta_1.JPG',
+        descripcion: 'Oportunidad condominio el Coigue gran casa remodelada con espectacular quinco y piscina.',
+        ubicacion: 'La Montura, Puente Alto, Chile, Las Vizcachas, Puente Alto, RM (Metropolitana)',
+        habitaciones: 4,
+        bano: 2,
+        costo: 6090,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Casa En Venta De 3 Dorm. En Puente Alto',
+        src: './assets/img/venta_2.JPG',
+        descripcion: 'Casa de dos pisos en Micro barrio en sector Las vizcachas, 68m² útiles / 165m² terreno total, con 3 dormitorios, 2 baños, living, comedor. Clóset en todas sus piezas, estacionamientos para 2 o 3 vehículos.',
+        ubicacion: 'Portezuelo Del Cepo, Las Vizcachas, Puente Alto, RM (Metropolitana)',
+        habitaciones: 3,
+        bano: 1,
+        costo: 4200,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Preciosa Casa En Tranquilo Pasaje, Muy Conectada',
+        src: './assets/img/venta_3.JPG',
+        descripcion: 'Segura y conectada, cercana a: colegios, metro, locomoción colectiva, mall vivo, centro de formación técnica, supermercados Jumbo y Hospital Sotero del Río, dentro de los múltiples servicios cercanos.',
+        ubicacion: 'El Trevo 3860, 8221088 Puente Alto, Región Metropolitana, Chile, Elisa Correa, Puente Alto, RM (Metropolitana)',
+        habitaciones: 3,
+        bano: 2,
+        costo: 4900,
+        smoke: true,
+        pets: false
+    },
+    {
+        nombre: 'Departamento',
+        src: './assets/img/venta_4.JPG',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        ubicacion: 'Lorem ipsum dolor sit amet 3860, 8221088 Puente Alto, Región Metropolitana, Chile',
+        habitaciones: 2,
+        bano: 1,
+        costo: 2200,
+        smoke: false,
+        pets: true
+    },
+    {
+        nombre: 'Departamento',
+        src: './assets/img/venta_5.JPG',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        ubicacion: 'Lorem ipsum dolor sit amet 3860, 8221088 Puente Alto, Región Metropolitana, Chile',
+        habitaciones: 5,
+        bano: 3,
+        costo: 12200,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Departamento',
+        src: './assets/img/venta_6.JPG',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        ubicacion: 'Lorem ipsum dolor sit amet 3860, 8221088 Puente Alto, Región Metropolitana, Chile',
+        habitaciones: 5,
+        bano: 3,
+        costo: 27800,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Departamento',
+        src: './assets/img/venta_7.JPG',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        ubicacion: 'Lorem ipsum dolor sit amet 3860, 8221088 Puente Alto, Región Metropolitana, Chile',
+        habitaciones: 3,
+        bano: 2,
+        costo: 8800,
+        smoke: false,
+        pets: true
+    }
+];
+
+function crearPropiedad(_propiedad) {
+    /* creacion div principal */
+    const nuevo = document.createElement('div');
+    nuevo.className = 'propiedad';
+    /* creacion imagen */
+    const img_contenedor = document.createElement('div');
+    img_contenedor.className = 'img';
+
+    const img = document.createElement('img');
+    img.src = _propiedad.src;
+
+    img_contenedor.appendChild(img);
+    nuevo.appendChild(img_contenedor);
+
+    /* Creacion de div informacion */
+    const informacion = document.createElement('div');
+    informacion.className = 'informacion';
+
+    /* creacion de nombre */
+    const name = document.createElement('h3');
+    name.innerText = _propiedad.nombre;
+    informacion.appendChild(name);
+
+    /* Creacion de descripcion */
+    const descripcion = document.createElement('h5');
+    descripcion.innerText = _propiedad.descripcion;
+    informacion.appendChild(descripcion);
+
+    /* creacion de ubicacion con icono */
+    const ubicacion = document.createElement('h5');
+    const icono_ubicacion = document.createElement('i');
+    icono_ubicacion.className = 'fa-solid fa-location-dot';
+    ubicacion.appendChild(icono_ubicacion);
+    ubicacion.append(_propiedad.ubicacion);
+    informacion.appendChild(ubicacion);
+
+    /* creacion de dormitorios y baños */
+    const habBan =  document.createElement('h5');
+
+    const icono_cama = document.createElement('i');
+    icono_cama.className = 'fa-solid fa-bed';
+    habBan.appendChild(icono_cama);
+
+    let habitacion;
+
+    if(_propiedad.habitaciones === 1) {
+        habitacion = `${_propiedad.habitaciones} Habitación | `;
+    } else {
+        habitacion = `${_propiedad.habitaciones} Habitaciones | `;
+    }
+        
+    habBan.append(habitacion);
+
+    const icono_bano = document.createElement('i');
+    icono_bano.className = 'fa-solid fa-bath';
+
+    habBan.append(icono_bano);
+
+    let banos;
+
+    if(_propiedad.bano === 1) {
+        banos = `${_propiedad.bano} Baño`;
+    } else {
+        banos = `${_propiedad.bano} Baños`;
+    }
+
+    habBan.append(banos);
+    informacion.appendChild(habBan);
+
+    const price = document.createElement('h2');
+
+/* fomtear el precio */
+    price.innerText = new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(
+        _propiedad.costo
+      ) ;
+
+    informacion.appendChild(price);
+
+    const petHtml = document.createElement('h5');
+
+    if(_propiedad.pets === true) {
+        petHtml.className = 'pet';
+        const icon_pet = document.createElement('i');
+        icon_pet.className = 'fa-solid fa-paw';
+        petHtml.appendChild(icon_pet);
+        petHtml.append('Mascotas permitidas');
+    } else {
+        petHtml.className = 'not-pet';
+        const icon_pet = document.createElement('i');
+        icon_pet.className = 'fa-solid fa-ban';
+        petHtml.appendChild(icon_pet);
+        petHtml.append('No se permiten mascotas');
+    }
+
+    informacion.appendChild(petHtml);
+
+    const smokeHtml = document.createElement('h5');
+
+    if(_propiedad.smoke === true) {
+        smokeHtml.className = 'smoke';
+        const icon_pet = document.createElement('i');
+        icon_pet.className = 'fa-solid fa-smoking';
+        smokeHtml.appendChild(icon_pet);
+        smokeHtml.append('Permitido fumar');
+    } else {
+        smokeHtml.className = 'not-smoke';
+        const icon_pet = document.createElement('i');
+        icon_pet.className = 'fa-solid fa-ban-smoking';
+        smokeHtml.appendChild(icon_pet);
+        smokeHtml.append('No se permite fumar');
+    }
+
+    informacion.appendChild(smokeHtml);
+    nuevo.appendChild(informacion);
+
+    return nuevo;
+}
+
+const alquiler_summary = document.getElementById("alquiler-summary");
+
+if(alquiler_summary){
+    for (let index = 0; index < 3; index++) {
+        const propiedad = crearPropiedad(propiedades_alquiler[index]);
+        alquiler_summary.appendChild(propiedad);
+    }
+}
+
+const ventas_summary = document.getElementById("ventas-summary");
+
+if(alquiler_summary){
+    for (let index = 0; index < 3; index++) {
+        const propiedad = crearPropiedad(propiedades_venta[index]);
+        ventas_summary.appendChild(propiedad);
+    }
+}
+
+const ventas = document.getElementById("ventas");
+
+if(ventas){
+    for (let index = 0; index < propiedades_venta.length; index++) {
+        const propiedad = crearPropiedad(propiedades_venta[index]);
+        ventas.appendChild(propiedad);
+    }
+}
+
+const alquiler = document.getElementById("alquiler");
+
+if(alquiler){
+    for (let index = 0; index < propiedades_alquiler.length; index++) {
+        const propiedad = crearPropiedad(propiedades_alquiler[index]);
+        alquiler.appendChild(propiedad);
+    }
+}
